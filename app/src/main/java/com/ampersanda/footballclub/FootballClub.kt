25ad3b80566d64a174834f312330624f5c80e903
Mochamad Lucky Pradana <luckypradana123@yandex.com>
2018-10-18 12:47:56 +0700

    use @parcelize

--- app/src/main/java/com/ampersanda/footballclub/FootballClub.kt
+++ app/src/main/java/com/ampersanda/footballclub/FootballClub.kt
@@ -1,28 +1,7 @@
 package com.ampersanda.footballclub
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class FootballClub(val name: String, val image: Int, val description: String) : Parcelable {
-    constructor(source: Parcel) : this(
-            source.readString(),
-            source.readInt(),
-            source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(name)
-        writeInt(image)
-        writeString(description)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<FootballClub> = object : Parcelable.Creator<FootballClub> {
-            override fun createFromParcel(source: Parcel): FootballClub = FootballClub(source)
-            override fun newArray(size: Int): Array<FootballClub?> = arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class FootballClub(val name: String, val image: Int, val description: String) : Parcelable
